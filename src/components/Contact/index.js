3237013fc535d80c4968c3f7f9505db7b2b49e71
Ashley <apark8496@gmail.com>
2022-03-27 23:08:43 -0400

    skill-icons added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useRef } from "react";
-import validateEmail from "../../../utils/helpers";
+import validateEmail from "../utils/helpers";
 import emailjs from "@emailjs/browser";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
 
 function Contact () {
 
