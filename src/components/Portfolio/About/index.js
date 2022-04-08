20ff1d4c8495842bfd7098bd01c2a127f823af6e
Ashley <apark8496@gmail.com>
2022-04-08 08:45:57 -0400

    momo the cat gif added

--- src/components/Portfolio/About/index.js
+++ src/components/Portfolio/About/index.js
@@ -2,35 +2,44 @@
 import React, { useEffect, useState } from 'react';
 
 function About() {
-	const githubEndpoint = 'https://api.github.com/users/apark8496';
-	const [githubData, setGithubData] = useState('');
+    const githubEndpoint = 'https://api.github.com/users/apark8496';
+    const [githubData, setGithubData] = useState('');
 
     // get information from github --> profile pic
-	useEffect(() => {
-		getGithubData();
-	}, []);
+    useEffect(() => {
+        getGithubData();
+    }, []);
 
-	const getGithubData = async () => {
-		try {
-			const res = await fetch(githubEndpoint);
-			const data = await res.json();
-			setGithubData(data);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+    const getGithubData = async () => {
+        try {
+            const res = await fetch(githubEndpoint);
+            const data = await res.json();
+            setGithubData(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     return (
         <section>
-            <h2 className='heading'>Hello! I'm Ashley.</h2>
-            <h3 className='sub-heading'>Full Stack Web Developer</h3>
+            <h2 className='heading'>Hello! I'm Ashley</h2>
+            <h3 className='sub-heading'>Full Stack Web Developer <br />
+            <img
+                    className='momo-gif'
+                    src="https://64.media.tumblr.com/b1ad91b5fa08a0723d0c18db6d7fb6a7/tumblr_mmbekl0nCq1rfjowdo1_500.gif"
+                    width='50'
+                    height='50'
+                    alt="momo"
+                />
+            </h3>
+
             <div className='bio-container'>
                 {/* profile picture */}
-               <img
-					className='my-pic'
-					src={githubData.avatar_url}
-					alt='Ashley Park.'
-				/>
+                <img
+                    className='my-pic'
+                    src={githubData.avatar_url}
+                    alt='Ashley Park.'
+                />
                 {/* about section */}
                 <aside className='bio'>
                     I'm a web developer from North Carolina, and currently live with my husband and two cats. <br />
@@ -44,7 +53,7 @@ function About() {
                     such as JavaScript, React, HTML, and CSS as well as back-end <br />
                     tools like Node.js, Express.js, MongoDB, GraphQL and MySQL <br />
                     while following to development methods such as <br />
-                    RESTful APIs, MVC structure and Single Page 
+                    RESTful APIs, MVC structure and Single Page
                     Applications (SPAs).
                     <br />
                     {/* insert section on hobbies */}
