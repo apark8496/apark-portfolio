c4fe8ff8bfe470c0683a2eccc925b2d1a2f4f27e
Ashley <apark8496@gmail.com>
2022-05-16 15:27:02 -0400

    fixed footer issue

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,14 @@ import { LINKS } from '../../utils/footerLinks.js';
 
 function Footer() {
 	return (
-		<footer className='footer'>
-			<Icon links={LINKS}></Icon>
-			<p> © 2022 Ashley Park</p>
-		</footer>
+		<div className='footer-basic'>
+			<footer>
+				<div className='social'>
+					<Icon className='icon' links={LINKS}></Icon>
+				</div>
+				<p className='copyright'> © 2022 Ashley Park</p>
+			</footer>
+		</div>
 	);
 }
 
