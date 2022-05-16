3977f2d85619cb5c64b893113f7af54cc8468fe0
Ashley <apark8496@gmail.com>
2022-05-16 11:14:10 -0400

    updates to resume link

--- src/components/Portfolio/Resume/index.js
+++ src/components/Portfolio/Resume/index.js
@@ -20,7 +20,7 @@ function Resume() {
 				View or download my full resume
 				<a
 					className='resume-link'
-					href='https://docs.google.com/document/d/1rg81NgjSfpK3jqAOJh4slO0DzSLXiLD5SseVYiI0kLE/edit?usp=sharing'
+					href='https://drive.google.com/file/d/14sbE4Gop9gKiwYIMNXIYZ8Y0XOnK-Edj/view?usp=sharing'
 					target="_blank"
 					rel="noreferrer"
 				>
