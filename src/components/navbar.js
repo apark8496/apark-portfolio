1eacfbed6ddf8a816656e27df7cccdeb06e073c0
Ashley <apark8496@gmail.com>
2022-07-13 06:04:09 -0400

    working on minor css styles under skills

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,19 +11,20 @@ function Navigation() {
 				<Navbar collapseOnSelect expand="sm" variant="light">
 					<Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
 					<Navbar.Collapse id="navbarScroll">
-						<Nav className="ml-auto">
+						<Nav className="ml-auto" defaultActiveKey="/apark-portfolio">
 							<Navbar.Brand href="/apark-portfolio">
 								<h1 className="AP-logo">AP</h1>
 							</Navbar.Brand>
-							<Nav.Item className="nav-item">
+							<Nav.Item>
 								<NavLink className="nav-link"
 									eventKey="1"
 									as={Link}
-									to="/apark-portfolio">
+									to="/apark-portfolio"
+									>
 									About
 								</NavLink>
 							</Nav.Item>
-							<Nav.Item className="nav-item">
+							<Nav.Item>
 								<NavLink className="nav-link"
 									eventKey="2"
 									as={Link}
@@ -32,7 +33,7 @@ function Navigation() {
 									Projects
 								</NavLink>
 							</Nav.Item>
-							<Nav.Item className="nav-item">
+							<Nav.Item>
 								<NavLink className="nav-link"
 									eventKey="3"
 									as={Link}
@@ -41,7 +42,7 @@ function Navigation() {
 									Contact
 								</NavLink>
 							</Nav.Item>
-							<Nav.Item className="nav-item">
+							<Nav.Item>
 								<NavLink className="nav-link"
 									eventKey="4"
 									as={Link}
